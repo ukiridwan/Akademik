920a87cfe12884395cf3ddbb0c4c37998ff8c7e1
UKI <UKI@UKI-PC>
2016-11-07 14:25:19 +0700

    sa

--- src/Model/Mahasiswa.java
+++ src/Model/Mahasiswa.java
@@ -5,10 +5,76 @@
  */
 package Model;
 
+import Controller.*;
+
 /**
  *
- * @author UKI
+ * @author Toms
  */
-class Mahasiswa {
+public class Mahasiswa {
+    private String namaMhs;
+    private String nim;
+    private String token;
+    private String kelas;
+    private String alamatMhs;
+    private String telpMhs;
+    private String passMhs;
+
+    public String getNamaMhs() {
+        return namaMhs;
+    }
+
+    public void setNamaMhs(String namaMhs) {
+        this.namaMhs = namaMhs;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getKelas() {
+        return kelas;
+    }
+
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+
+    public String getAlamatMhs() {
+        return alamatMhs;
+    }
+
+    public void setAlamatMhs(String alamatMhs) {
+        this.alamatMhs = alamatMhs;
+    }
+
+    public String getTelpMhs() {
+        return telpMhs;
+    }
+
+    public void setTelpMhs(String telpMhs) {
+        this.telpMhs = telpMhs;
+    }
+
+    public String getPassMhs() {
+        return passMhs;
+    }
+
+    public void setPassMhs(String passMhs) {
+        this.passMhs = passMhs;
+    }
+
     
 }
